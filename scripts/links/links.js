c6e64d0eff01289c405376fd25c8e1768f8182a7
lobaevsl <lobaevlva@gmail.com>
2023-05-22 12:49:32 +0300

    update

--- scripts/links/links.js
+++ scripts/links/links.js
@@ -1,17 +1,24 @@
 const regExp = new RegExp('([^, ]+)', 'g');
 
-function split(){
-    const input = document.getElementById('links').value;
-    const links = [...input.matchAll(regExp)];
-    let o = '';
-    for(let i = 0; i < links.length; i++){
-        let elem = document.createElement('a');
-        elem.href = links[i][0];
-        elem.target = '_blank';
-        elem.textContent = 'Ссылка ' + (i + 1);
-        elem.appendChild(document.createElement('br'));
+function split() {
+    const input = document.getElementById('links');
+    const output = document.getElementById('out');
 
-        document.getElementById('out').appendChild(elem);
+    output.innerHTML = '';
+    const links = [...input.value.matchAll(regExp)];
+    if (links.length === 1) {
+        output.innerText = 'Ответ';
+    } else {
+        for (let i = 0; i < links.length; i++) {
+            let elem = document.createElement('a');
+            elem.href = links[i][0];
+            elem.target = '_blank';
+            elem.textContent = 'Ссылка ' + (i + 1);
+            elem.appendChild(document.createElement('br'));
+
+            output.appendChild(elem);
+        }
     }
+    input.value = '';
 
 }
