0a5092430bdc7ed8c75e7d5cfaacd7ba15837ac8
lobaevsl <lobaevlva@gmail.com>
2023-05-25 17:24:31 +0300

    Create last4digits.html and last4digits.js Edit style.css Add link from index.html

---
+++ scripts/last4digits/last4digits.js
@@ -0,0 +1,31 @@
+const regExp = new RegExp('\\**((\\d){1,4})(\n|$)', 'g');
+
+function check() {
+    const input = document.getElementById('digits');
+    const output = document.getElementById('out');
+
+    const nums = [...input.value.matchAll(regExp)];
+    if (nums.length === 0) {
+        output.innerText = '';
+    } else {
+        output.rows = nums.length;
+        let outputNums = '';
+        for (let i = 0; i < nums.length; i++){
+            let num = nums[i][1];
+            let outputNum = '';
+            if (num.length < 4){
+                for (let x = 0; x < 4 - num.length; x++){
+                    outputNum += 0;
+                }
+            }
+            outputNums += '*' + outputNum + num + '\n';
+        }
+        outputNums = outputNums.slice(0, -1);
+        output.innerHTML = outputNums;
+    }
+}
+
+function copyToClipboard(){
+    const output = document.getElementById('out');
+    navigator.clipboard.writeText(output.innerHTML).then(() => {});
+}
